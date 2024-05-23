5b46e99e4119b16cfa209e962be13476983ae430
matheus-olie <matheus.oliveiraallves@gmail.com>
2024-05-22 22:54:07 -0300

    projeto conta banco finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -2,7 +2,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         //criando o objeto scanner
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
